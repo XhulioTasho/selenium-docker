f7784637080ecb108f675b688b7bd07c5d385bea
xhuliotasho <xhulio.tasho@gmail.com>
2020-04-23 15:51:43 +0200

    Change Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM openjdk:8u191-jre-alpine3.8
 RUN apk add curl jq
 
 # Workspace
-WORKDIR /usr/share/udemy
+WORKDIR C:\Users\AG\Downloads\workspace
 
 # ADD .jar under target from host
 # into this image
--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package com.tests;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
@@ -9,6 +10,7 @@ import org.testng.annotations.BeforeTest;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
@@ -19,8 +21,9 @@ public class BaseTest {
         // BROWSER => chrome / firefox
         // HUB_HOST => localhost / 10.0.1.3 / hostname
 
-        String host = "localhost";
-        DesiredCapabilities dc;
+        //String host = "localhost";
+        String host = "178.128.205.150" ;
+        DesiredCapabilities dc ;
 
         if(System.getProperty("BROWSER") != null &&
                 System.getProperty("BROWSER").equalsIgnoreCase("firefox")){
@@ -38,13 +41,15 @@ public class BaseTest {
         String completeUrl = "http://" + host + ":4444/wd/hub";
         dc.setCapability("name", testName);
         this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
+
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\AG\\Desktop\\SUMMER\\selenium-docker\\Browser\\chromedriver.exe");
+        //driver=new ChromeDriver();
+        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.manage().window().maximize();
     }
 
     @AfterTest
     public void quitDriver(){
         this.driver.quit();
     }
-
-
-
 }
